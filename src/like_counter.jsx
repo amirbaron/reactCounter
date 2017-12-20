f01cc94af5755c6fdf3353c6f1cd4b38494eb817
Amir Baron <amirb@dropbox.com>
2017-12-20 14:25:55 +0200

    progress

--- src/like_counter.jsx
+++ src/like_counter.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import $ from "jquery";
 
 export class LikeCounter extends Component {
 
@@ -8,7 +9,9 @@ export class LikeCounter extends Component {
         // “fork” props to state
         super(props);
         console.log("constructor");
-        this.state = {counter: this.props.counter};
+        this.state = {counter: this.props.counter, priceUsd: 0};
+        this.mounted = false
+
     }
 
     componentWillMount() {
@@ -18,11 +21,24 @@ export class LikeCounter extends Component {
         console.log("componentWillMount");
     }
 
+    getBitCoinPrice = () => {
+        if (this.mounted) {
+            $.get('https://poloniex.com/public?command=returnTicker').done((res) => {
+                console.log(res);
+                let bitCoinPrice = res.USDT_BTC.last;
+                this.setState({priceUsd: bitCoinPrice})
+            })
+            window.setTimeout(this.getBitCoinPrice, 5000);
+        }
+    }
+
     componentDidMount() {
         // is invoked immediately after a component is mounted.
         // Initialization that requires DOM nodes should go here.
         // If you need to load data from a remote endpoint,
         // this is a good place to instantiate the network request
+        this.mounted = true;
+        this.getBitCoinPrice();
         console.log("componentDidMount");
     }
 
@@ -58,6 +74,7 @@ export class LikeCounter extends Component {
         //is invoked immediately before a component is unmounted and destroyed. Perform any necessary cleanup in this method,
         // such as invalidating timers, canceling network requests,
         console.log('componentWillUnmount');
+        this.mounted = false;
     }
 
     onLike = () => {
@@ -67,7 +84,8 @@ export class LikeCounter extends Component {
     render() {
         console.log("Render")
         return (<span>
-            <span>Liked {this.state.counter} </span>
+            <span className="price"> Bitcoin price {this.state.priceUsd}</span>
+            <span>Liked {this.state.counter} times</span>
             <span>
             <button onClick={this.onLike}>
                 Like!
